12c8063a243424256c544a0f3fec553a51ca555d
MarucoMass <mariomass23@gmail.com>
2022-12-13 03:52:47 -0300

    testing added

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('test App component', () => {
+		beforeEach( () => {
+			render(<App/>);
+		})
+	
+		test('testing', () => {
+			expect(true).toBe(true);
+		})
+	
+	})
